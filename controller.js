e238c8b6df4ec4aa2effcce67822a9da4abca96b
nach9 <nur.achdiansyah@gmail.com>
2017-10-01 22:47:00 +0700

    done

--- controller.js
+++ controller.js
@@ -1,4 +1,73 @@
+'use strict'
+
+const Model = require('./model')
+const View = require ('./view')
+
 class Controller {
+  constructor(deck){
+    this.deck = deck
+    this.answer=[]
+    this.wrong=[]
+    this.index=0
+  }
+
+  noDeck(){
+    View.noDeck()
+  }
+
+  nextIndex(index){
+    let quiz = Model.getData(this.deck)
+    if(this.wrong.length==3){ //3x salah game over
+      View.showGameover(this.answer.length - this.wrong.length , this.wrong.length)
+      return
+    } else {
+      while(this.answer.length < quiz.length){ //mencari index yg belum dijawab
+        if ( index >= quiz.length)  { // kembali ke index awal
+          index=0
+        }
+        if(this.answer.indexOf(index)!=-1){ //jika ditemukan cari next index
+          index++
+        }
+        else{
+          return index
+        }
+      }
+    }
+
+  }
+
+  showQuiz(index){
+    let quiz = Model.getData(this.deck)
+    if(quiz[index]){
+      this.index = index
+      View.showQuiz(quiz[this.index],(answer)=>{
+          let quiz = Model.getData(this.deck)
+          if (answer=='skip'){ //skip ke next index
+            this.index ++
+            this.index = this.nextIndex(this.index)
+            this.showQuiz(this.index )
+          } else {
+            if (quiz[this.index].term==answer){
+              View.showBenar()
+              this.answer.push(this.index)
+              this.index = this.nextIndex(this.index)
+              this.showQuiz(this.index )
+            }
+            else if(quiz[this.index].term!=answer){
+              this.answer.push(this.index)
+              this.wrong.push(this.index)
+              View.showSalah(this.wrong.length)
+              this.index = this.nextIndex(this.index)
+              this.showQuiz(this.index )
+            }
+          }
+      })
+    }
+    if(this.answer.length==quiz.length){ //jika pertanyaan habis
+      View.showFinish( this.answer.length - this.wrong.length , this.wrong.length)
+    }
+  }
+
 
 }
 
--- view.js
+++ view.js
@@ -1,5 +1,46 @@
-class View {
+'use strict'
 
+const readline = require('readline')
+const rl = readline.createInterface({input: process.stdin,output: process.stdout});
+
+
+class View {
+  static showQuiz(quiz,callback){
+    rl.question(`${quiz.definition}\n`, (answer)=>{
+        callback(answer)
+    })
+  }
+
+  static noDeck(){
+    console.log("Please input choice deck");
+    rl.close()
+  }
+
+  static showBenar(){
+    console.log('Cendol Gan');
+    console.log('=================================');
+  }
+
+  static showSalah(salah){
+    console.log(`Bata merah Gan ... udah ${salah} kali salah`);
+    console.log('=================================');
+  }
+
+  static showGameover(benar,salah){
+    console.log('GAME OVER');
+    console.log('=================================');
+    console.log(`Total Benar : ${benar}`);
+    console.log(`Total Salah : ${salah}`);
+    rl.close()
+  }
+
+  static showFinish(benar , salah){
+    console.log('END');
+    console.log('=================================');
+    console.log(`Total Benar : ${benar}`);
+    console.log(`Total Salah : ${salah}`);
+    rl.close()
+  }
 }
 
 module.exports = View
