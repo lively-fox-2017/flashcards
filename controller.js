90aa2a46e8898174cb0596048f9296dbf0e9b858
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-01 09:23:29 +0700

    create model, view, and controller for flashcard

--- controller.js
+++ controller.js
@@ -1,5 +1,55 @@
-class Controller {
+"use strict"
 
+const Model = require('./model');
+const View = require('./view');
+
+class Controller {
+	static start(deck) {
+		return Model.initGame(deck)
+					.then(() => {
+						View.welcomeMessage();
+					});
+	}
+
+	static draw(session) {
+		return new Promise(resolve => {
+			let newSession = Model.randomizeQuestion(session);
+			View.showQuestion(newSession, obj => {
+				newSession = Model.evaluateAnswer(obj);
+				Model.writeSession(newSession);
+			});
+		});
+	}
+
+	static play() {
+		let newSession;
+
+		return Model.readSession()
+					.then(session => {
+						newSession = Model.randomizeQuestion(session);
+						return Model.evaluateStatus(newSession);
+					})
+					.then(newSession => {
+						return View.showQuestion(newSession);
+					})
+					.then(obj => {
+						let result = Model.evaluateAnswer(obj);
+						newSession = result[0];
+
+						if (result[1] === 'skipped') View.jawabanSkip(newSession);
+						else if (result[1] === 'benar') View.jawabanBenar(newSession);
+						else View.jawabanSalah(newSession);
+						return Model.writeSession(newSession);
+					})
+					.then(() => {
+						return this.play();
+					})
+					.catch(session => {
+						if (session.isGameOver || session.isNoMoreQuestion) {
+							return View.closingMessage(session);
+						}
+					});
+	}
 }
 
-module.exports = Controller
+module.exports = Controller;
